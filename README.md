db41d4ec1ad22852d0e100d314ff5480db96738f
nevertheone <111437544+nevertheone@users.noreply.github.com>
2022-12-06 13:23:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TourOfHeroes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pwrhfs)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
